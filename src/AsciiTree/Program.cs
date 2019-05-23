2a9502d15ff765e828254e3af77ca7ad4ebf320a
congzw <2981810300@qq.com>
2019-05-23 14:51:01 +0800

    fix lower case bug

--- src/AsciiTree/Program.cs
+++ src/AsciiTree/Program.cs
@@ -10,7 +10,7 @@ namespace Demos
         static void Main(string[] args)
         {
             var asciiTreeHelper = new AsciiTreeHelper();
-            var appendRoot = !Assembly.GetExecutingAssembly().CodeBase.ToLower().Contains("_NoRoot");
+            var appendRoot = !Assembly.GetExecutingAssembly().CodeBase.ToLower().Contains("_noroot");
             var asciiTreeText = asciiTreeHelper.GetCurrentDirectoryAsciiTreeText(appendRoot);
             Console.WriteLine(asciiTreeText);
             File.WriteAllText("AsciiTreeText.txt", asciiTreeText);
